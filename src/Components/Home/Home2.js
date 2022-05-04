2dda605caba4cbfe88222b36eed2003152cc0176
KhadijaBarhoumi <khadijabarhoumi15@gmai.com>
2022-05-05 01:02:36 +0200

    my commit

--- src/Components/Home/Home2.js
+++ src/Components/Home/Home2.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/v.png";
 import Logo from "../../Assets/logo.png";
 import Tilt from "react-parallax-tilt";
 import { ImPointRight } from "react-icons/im";
