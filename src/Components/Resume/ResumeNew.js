4e4178e388927962f59ad8fe0d6e8e6cbffc38ed
KhadijaBarhoumi <khadijabarhoumi15@gmai.com>
2023-04-13 00:47:48 +0200

    resume

--- src/Components/Resume/ResumeNew.js
+++ src/Components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Resume.pdf";
+import pdf from "../../Assets/Khadija Resume.pdf";
 import { AiOutlineDownload,AiFillBank } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/KhadijaBarhoumi/MyPortfolio/blob/main/src/Assets/khadijabarhoumiCV.pdf";
+  "https://github.com/KhadijaBarhoumi/MyPortfolio/blob/main/src/Assets/Khadija%20Resume.pdf";
 
 function Resume() {
   const [width, setWidth] = useState(1200);
